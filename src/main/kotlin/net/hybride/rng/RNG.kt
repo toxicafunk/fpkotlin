74f91b1a01a287684537e267070b1a7fd698f9c5
ericnoam <eric.rodriguez@leovegas.com>
2022-10-20 10:01:10 +0200

    Roll die with API

--- src/main/kotlin/net/hybride/rng/RNG.kt
+++ src/main/kotlin/net/hybride/rng/RNG.kt
@@ -134,3 +134,4 @@ fun <A, B, C> map2F(ra: Rand<A>, rb: Rand<B>, f: (A, B) -> C): Rand<C> =
 fun rollDie(): Rand<Int> = nonNegativeIntLessThan(6)
 
 fun rollDieFix(): Rand<Int> = map(nonNegativeIntLessThan(6)) { it + 1 }
+
