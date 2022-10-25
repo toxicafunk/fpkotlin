b5684fc5615e9d3ee7474531bf290d06d0adb4b5
toxicafunk <toxicafunk@gmail.com>
2022-10-26 00:43:01 +0200

    More Par functions

---
+++ src/main/kotlin/net/hybride/concurrent/Pars.kt
@@ -0,0 +1,114 @@
+package net.hybride.concurrent
+
+import java.util.concurrent.TimeUnit
+//import java.util.concurrent.ExecutorService
+
+interface Callable<A> {
+    fun call(): A
+}
+
+interface Future<A> {
+    fun get(): A
+    fun get(timeout: Long, timeUnit: TimeUnit): A
+    fun cancel(evenIfRunning: Boolean): Boolean
+    fun isDone(): Boolean
+    fun isCancelled(): Boolean
+}
+
+interface ExecutorService {
+    fun <A> submit(c: Callable<A>): Future<A>
+}
+
+typealias Par<A> = (ExecutorService) -> Future<A>
+
+fun <A> run(es: ExecutorService, a: Par<A>): Future<A> = a(es)
+
+object Pars {
+    fun <A> unit(a: A): Par<A> = {
+            es: ExecutorService -> UnitFuture(a)
+    }
+
+    data class UnitFuture<A>(val a: A) : Future<A> {
+        override fun get(): A = a
+        override fun get(timeout: Long, timeUnit: TimeUnit): A = a
+        override fun cancel(evenIfRunning: Boolean): Boolean = false
+        override fun isDone(): Boolean = true
+        override fun isCancelled(): Boolean = false
+    }
+
+    fun <A, B, C> map2Unit(
+        a: Par<A>,
+        b: Par<B>,
+        f: (A, B) -> C
+    ): Par<C> = {
+            es: ExecutorService ->
+        val af: Future<A> = a(es)
+        val bf: Future<B> = b(es)
+        UnitFuture(f(af.get(), bf.get()))
+    }
+
+    fun <A> fork(
+        a: () -> Par<A>
+    ): Par<A> = {
+            es: ExecutorService ->
+        es.submit(Callable<A> { a()(es).get() })
+    }
+
+    data class TimedMap2Future<A, B, C>(
+        val pa: Future<A>,
+        val pb: Future<B>,
+        val f: (A, B) -> C
+    ) : Future<C> {
+        override fun isDone(): Boolean = TODO("Unused")
+        override fun get(): C = TODO("Unused")
+        override fun get(to: Long, tu: TimeUnit): C {
+            val timeoutMillis = TimeUnit.MILLISECONDS.convert(to, tu)
+            val start = System.currentTimeMillis()
+            val a = pa.get(to, tu)
+            val duration = System.currentTimeMillis() - start
+            val remainder = timeoutMillis - duration
+            val b = pb.get(remainder, TimeUnit.MILLISECONDS)
+            return f(a, b)
+        }
+        override fun cancel(b: Boolean): Boolean = TODO("Unused")
+        override fun isCancelled(): Boolean = TODO("Unused")
+    }
+
+    fun <A, B, C> map2(
+        a: Par<A>,
+        b: Par<B>,
+        f: (A, B) -> C
+    ): Par<C> = {
+            es: ExecutorService ->
+        val fa: Future<A> = a(es)
+        val fb: Future<B> = b(es)
+        TimedMap2Future(fa, fb, f)
+    }
+
+    fun <A> lazyUnit(a: () -> A): Par<A> =
+        Pars.fork { Pars.unit(a()) }
+
+    fun <A, B> asyncF(f: (A) -> B): (A) -> Par<B> = { a ->
+        lazyUnit { f(a) }
+    }
+
+    fun sortParNaive(parList: Par<List<Int>>): Par<List<Int>> =
+        map2(parList, unit(Unit)) { a, _ -> a.sorted() }
+
+    fun <A, B> map(pa: Par<A>, f: (A) -> B): Par<B> =
+        map2(pa, unit(Unit), { a, _ -> f(a) })
+
+    fun sortPar(parList: Par<List<Int>>): Par<List<Int>> =
+        map(parList) { it.sorted() }
+
+    fun <A, B> parMap(
+        ps: List<A>,
+        f: (A) -> B
+    ): Par<List<B>> = {
+        val fbs: List<Par<B>> = ps.map(asyncF(f))
+        TODO()
+    }
+
+    fun <A> sequence(ps: List<Par<A>>): Par<List<A>> =
+        TODO()
+}
