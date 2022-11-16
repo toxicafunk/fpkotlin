d301c9f8a699231141f37a6ad1a655b1e3dcbe55
toxicafunk <toxicafunk@gmail.com>
2022-11-16 01:34:25 +0100

    Some Gen combinators

--- src/main/kotlin/net/hybride/ppb/Gen.kt
+++ src/main/kotlin/net/hybride/ppb/Gen.kt
@@ -2,6 +2,9 @@ package net.hybride.ppb
 
 import net.hybride.Either
 import net.hybride.List
+import net.hybride.None
+import net.hybride.Option
+import net.hybride.Some
 import net.hybride.rng.nonNegativeInt
 import net.hybride.rng.nextBoolean
 import net.hybride.rng.RNG
@@ -34,12 +37,30 @@ data class Gen<A>(val sample: State<RNG, A>) {
     companion object {
         fun <A> listOfN(n: Int, ga: Gen<A>): Gen<List<A>> =
             Gen(State.sequence(List.fill(n) { ga.sample }))
+
+        fun <A> liftOption(ga: Gen<A>): Gen<Option<A>> =
+            ga.map { a -> if (a == null) None else Some(a) }
+
+        //fun <A> getOption(goa: Gen<Option<A>>): Gen<A> =
+        //    goa.flatMap { oa -> oa. }
     }
+
+    fun <B> flatMap(f: (A) -> Gen<B>): Gen<B> =
+        Gen(this.sample.flatMap { a -> f(a).sample })
+
+    fun <B> map(f: (A) -> B): Gen<B> =
+        flatMap { a -> unit(f(a)) }
+
     fun choose(start: Int, stopExclusive: Int): Gen<Int> =
         Gen(
             State { rng: RNG -> nonNegativeInt(rng) }
                 .map { start + (it % (stopExclusive - start)) }
         )
+
+    fun choosePair(start: Int, stopExclusive: Int): Gen<Pair<Int, Int>> =
+        choose(start, stopExclusive).flatMap { i1 ->
+            choose(start, stopExclusive).map { i2 -> i1 to i2 }
+        }
 
     fun chooseUnbiased(start: Int, stopExclusive: Int): Gen<Int> =
         Gen(
@@ -53,11 +74,14 @@ data class Gen<A>(val sample: State<RNG, A>) {
     fun boolean(): Gen<Boolean> =
         Gen(State { rng -> nextBoolean(rng) })
 
+    fun execute(rng: RNG): Pair<A, RNG> = this.sample.run(rng)
 
 }
 
 fun main() {
     val rng = SimpleRNG(404)
     val gen = Gen(State(intR))
-    println(Gen.listOfN(5, gen).sample.run(rng).first)
+    println(Gen.listOfN(5, gen).execute(rng).first)
+    println(gen.choose(10, 15).execute(rng).first)
+    println(gen.choosePair(20, 25).execute(rng).first)
 }
