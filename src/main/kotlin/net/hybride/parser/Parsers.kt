1169ad5f7c5d8bfe96d8b06cc6b9eb34f9709a87
ericnoam <eric.rodriguez@leovegas.com>
2022-12-12 14:29:37 +0100

    Parsers slice laws

--- src/main/kotlin/net/hybride/parser/Parsers.kt
+++ src/main/kotlin/net/hybride/parser/Parsers.kt
@@ -11,11 +11,13 @@ interface Parser<A>
 
 interface Parsers<PE> {
 
-    //tag::init4[]
-    fun char(c: Char): Parser<Char>
-    //end::init4[]
+    fun char(c: Char): Parser<Char> = string(c.toString()).map { it[0] }
 
     fun string(s: String): Parser<String>
+
+    fun <A> succeed(a: A): Parser<A> = string("").map { a }
+
+    fun <A> slice(pa: Parser<A>): Parser<String>
 
     fun <A> run(p: Parser<A>, input: String): Either<PE, A> = TODO()
 
@@ -35,11 +37,22 @@ interface Parsers<PE> {
 
     fun zeroPlusOnePlusChars(a: Char, b: Char, s: String): Either<PE, Parser<Pair<Int, Int>>>
 
-    //tag::init1[]
     fun <A> Parser<A>.many(): Parser<List<A>>
 
     fun <A,B> Parser<A>.map(f: (A) -> B): Parser<B>
-    //end::init1[]
+
+    fun <A> many1(p: Parser<A>): Parser<List<A>>
+
+    //fun <A, B> product(pa: Parser<A>, pb: Parser<B>): Parser<Pair<A, B>>
+
+    infix fun <A, B> Parser<A>.product(pb: Parser<B>): Parser<Pair<A, B>>
+
+    fun <A, B, C> map2(
+        pa: Parser<A>,
+        pb: () -> Parser<B>,
+        f: (A, B) -> C
+    ): Parser<C>
+
 
     fun charLaws(c: Char): Boolean =
      run(char(c), c.toString()) == Right(c)
@@ -75,6 +88,7 @@ abstract class Laws: Parsers<ParseError> {
 
     fun <A> mapLaw(p: Parser<A>, i: Gen<String>): Prop =
         equal(p, p.map { a: A -> a }, i)
+
 }
 //end::init3[]
 
@@ -84,4 +98,29 @@ abstract class Example : Parsers<ParseError> {
     fun numALaws(): Boolean = run(numA, "aaa") == Right(3)
             && run(numA, "b") == Right(0)
 
+    fun sliceLaws(): Boolean =
+        run(slice(("a" or "b").many()), "aaba") == Right("aaba")
+
+    override fun <A, B, C> map2(
+        pa: Parser<A>,
+        pb: () -> Parser<B>,
+        f: (A, B) -> C
+    ): Parser<C> =
+        (pa product pb()).map { pair -> f(pair.first, pair.second) }
+
+    override fun <A> many1(p: Parser<A>): Parser<List<A>> =
+        map2(p, { p.many() }) { a, b -> listOf(a) + b }
+
+    fun <A> many2(pa: Parser<A>): Parser<List<A>> =
+        or(
+            map2(pa, { pa.many() }) { a, la -> listOf(a) + la },
+            succeed(emptyList())
+        )
+
+    fun <A> listOfN1(n: Int, pa: Parser<A>): Parser<List<A>> =
+        if (n > 0)
+            map2(pa, { listOfN1(n - 1, pa) }) { a, la ->
+                listOf(a) + la
+            }
+        else succeed(emptyList())
 }
